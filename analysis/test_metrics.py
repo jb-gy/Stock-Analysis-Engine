15b389f9a8ce1816e990121d317a623f96bf0b5f
Ajibola Ganiyu <149854676+jb-gy@users.noreply.github.com>
2025-01-20 18:33:02 -0600

    Update test_metrics.py
    cleaning

--- analysis/test_metrics.py
+++ analysis/test_metrics.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def test_financial_metrics(symbols: list = ["AAPL", "MSFT", "GOOGL"]):
     """
-    TODO
+    
     Test financial metrics calculation for multiple stocks.
     
     Args:
